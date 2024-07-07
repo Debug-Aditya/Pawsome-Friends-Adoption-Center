f7170de7a6f8a6025ad5706dae40643822f67502
Debug-Aditya <patiladitya0109@gmail.com>
2024-07-07 10:23:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ form.addEventListener('submit', (e) => {
 
   if (username === 'Example' && password === '123') {
     // credentials are correct, redirect to index.html
-    window.location.href = 'index.html';
+    window.location.href = 'indexmain.html';
   } else {
     // credentials are incorrect, display an error message
     alert('Invalid username or password');
